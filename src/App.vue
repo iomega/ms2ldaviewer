a4d7c2b285cee7ca2b52df40805caad154f1459f
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2020-01-09 15:48:02 +0100

    Switched to quasar widget library

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,79 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Summary</router-link> |
-      <router-link to="/spectras">Fragment spectra</router-link> |
-      <router-link to="/motifs">Motifs</router-link> |
-      <router-link to="/settings">Settings</router-link> |
-      <router-link to="/pick">Pick a dataset</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          MS2LDA viewer
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item clickable exact to="/">
+          <q-item-section>
+            <q-item-label>Summary</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/spectras">
+          <q-item-section>
+            <q-item-label>Spectras</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/motifs">
+          <q-item-section>
+            <q-item-label>Motifs</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/settings">
+          <q-item-section>
+            <q-item-label>Settings</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/pick">
+          <q-item-section>
+            <q-item-label>Pick a dataset</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/about">
+          <q-item-section>
+            <q-item-label>About</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
 </template>
 
+<script>
+export default {
+  name: 'LayoutDefault',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
